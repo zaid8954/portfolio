146c6c8f22c0e14356ef507b992731d93790c93c
zaidkhan <zaidkhan@tradiasdigital.com>
2024-05-08 22:31:29 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,17 @@
+.project .overly {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-3);
+    visibility: visible;
+    opacity: 1;
+    transition: 0.5s;
+    transform: scale(0);
+    display: flr;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
